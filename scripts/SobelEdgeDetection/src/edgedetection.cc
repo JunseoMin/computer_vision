15b1d52b74a4212f706aca1394f9e4de04e6d4bc
JunsoMin <minjs4562@gmail.com>
2023-11-15 11:16:54 +0900

    [done] edgedetection done

--- scripts/SobelEdgeDetection/src/edgedetection.cc
+++ scripts/SobelEdgeDetection/src/edgedetection.cc
@@ -7,7 +7,7 @@ using namespace std;
 using namespace cv;
 
 void sobel_edge_detect(const Mat& input, Mat& output, bool heading);
-void sobel_edge_mask(const Mat& x_image, const Mat& y_image, Mat& output, const size_t _Threshold);
+void sobel_edge_mask(const Mat& x_image, const Mat& y_image, Mat& output, const int _Threshold);
 
 int main(void){
     Mat lena512;
@@ -22,37 +22,37 @@ int main(void){
     sobel_edge_detect(lena512, xSobel, true);
     sobel_edge_detect(lena512, ySobel, false);
 
-    Mat sobel_Edge;
+    Mat sobel_Edge(lena512.rows, lena512.cols, lena512.type());
 
-    Mat sobel_Edge_low;
-    Mat sobel_Edge_high;
+    Mat sobel_Edge_low(lena512.rows, lena512.cols, lena512.type());
+    Mat sobel_Edge_high(lena512.rows, lena512.cols, lena512.type());
 
     sobel_edge_mask(xSobel, ySobel, sobel_Edge, 0); //sobel edge(gx + gy)
 
-    sobel_edge_mask(xSobel, ySobel, sobel_Edge_low, 0); //sobel edge masking by low threshold = ?
-    sobel_edge_mask(xSobel, ySobel, sobel_Edge_high, 0); //sobel edge masking by high threshold = ?(higher threashold than before)
+    sobel_edge_mask(xSobel, ySobel, sobel_Edge_low, 30); //sobel edge masking by low threshold = ?
+    sobel_edge_mask(xSobel, ySobel, sobel_Edge_high, 100); //sobel edge masking by high threshold = ?(higher threashold than before)
 
-    // !! use CV function to apply gaussian filter !!
+    // // !! use CV function to apply gaussian filter !!
     Mat gaussian_filtered_lena;
     Mat gaussian_filtered_lena_high;
     GaussianBlur(lena512, gaussian_filtered_lena, Size(5, 5), 1.5);
     GaussianBlur(lena512, gaussian_filtered_lena_high, Size(5, 5), 3.);
 
-    Mat filtered_sobel;
-    Mat filtered_sobel_high;
+    Mat filtered_sobel(lena512.rows, lena512.cols, lena512.type());
+    Mat filtered_sobel_high(lena512.rows, lena512.cols, lena512.type());
 
-    Mat x;
-    Mat y;
+    Mat x(lena512.rows, lena512.cols, lena512.type());
+    Mat y(lena512.rows, lena512.cols, lena512.type());
 
     sobel_edge_detect(gaussian_filtered_lena, x, true);    //x
     sobel_edge_detect(gaussian_filtered_lena, y, false);    //y
 
-    sobel_edge_mask(x, y, filtered_sobel, 0);
+    sobel_edge_mask(x, y, filtered_sobel, 100);
     
     sobel_edge_detect(gaussian_filtered_lena_high, x, true);    //x
     sobel_edge_detect(gaussian_filtered_lena_high, y, false);    //y
     
-    sobel_edge_mask(x, y, filtered_sobel_high, 0);
+    sobel_edge_mask(x, y, filtered_sobel_high, 100);
     
     //imshows
     imshow("gx image", xSobel);
@@ -74,9 +74,13 @@ void sobel_edge_detect(const Mat& input, Mat& output, bool heading){  // true --
     // apply sobel mask to output by heading x or y axis
     Mat sobelKernel;
     if (heading) {
-        sobelKernel = (Mat_<int>(3, 3) << -1, 0, 1, -2, 0, 2, -1, 0, 1);
+        sobelKernel = (Mat_<int>(3, 3) << -1, 0, 1, 
+                                          -2, 0, 2,
+                                          -1, 0, 1);
     } else {
-        sobelKernel = (Mat_<int>(3, 3) << -1, -2, -1, 0, 0, 0, 1, 2, 1);
+        sobelKernel = (Mat_<int>(3, 3) << -1, -2, -1,
+                                           0, 0, 0, 
+                                           1, 2, 1);
     }
 
 
@@ -88,19 +92,19 @@ void sobel_edge_detect(const Mat& input, Mat& output, bool heading){  // true --
                     sum += input.at<uchar>(i + k, j + l) * sobelKernel.at<int>(1 + k, 1 + l);
                 }
             }
-            output.at<uchar>(i, j) = max(0,min(sum,255));
+            output.at<uchar>(i, j) = static_cast<uchar> (max(0,min(sum,255)));
         }
     }
 
 }
 
-void sobel_edge_mask(const Mat& x_image, const Mat& y_image, Mat& output, const size_t _Threshold){
+void sobel_edge_mask(const Mat& x_image, const Mat& y_image, Mat& output, const int _Threshold){
     // add sobel x and sobel y
     
     for (int i = 0; i < x_image.rows; ++i) {
         for (int j = 0; j < x_image.cols; ++j) {
-            int magnitude = sqrt(pow(x_image.at<uchar>(i, j), 2) + pow(y_image.at<uchar>(i, j), 2));
-            output.at<uchar>(i, j) = (magnitude > _Threshold) ? magnitude : 0;
+            uchar magnitude = static_cast<uchar>(x_image.at<uchar>(i, j) + y_image.at<uchar>(i, j));
+            output.at<uchar>(i, j) = min(255, (magnitude > _Threshold) ? magnitude : 0);
         }
     }
 
