176f5867f999d4caaf60e2b4ad8ffacb52279623
JunsoMin <minjs4562@gmail.com>
2023-11-24 17:51:22 +0900

    [fixed] affine matrix

--- scripts/ImageTransformation/src/imageTransformation.cc
+++ scripts/ImageTransformation/src/imageTransformation.cc
@@ -5,30 +5,26 @@
 using namespace cv;
 using namespace std;
 
-class TransformFunction
+class AffineTransform
 {
 private:
     Mat M;
 
 public:
-    TransformFunction(const vector<Point2f>& origin, const vector<Point2f>& transformed)
+    AffineTransform(const vector<Point2f>& origin, const vector<Point2f>& transformed)
     {
-        Mat A(8, 8, CV_64F, Scalar(0));
-        Mat B(8, 1, CV_64F);
+        Mat A(2 * origin.size(), 6, CV_64F, Scalar(0));
+        Mat B(2 * origin.size(), 1, CV_64F);
 
-        for (int i = 0; i < 4; ++i)
+        for (int i = 0; i < origin.size(); ++i)
         {
             A.at<double>(2 * i, 0) = origin[i].x;
             A.at<double>(2 * i, 1) = origin[i].y;
             A.at<double>(2 * i, 2) = 1;
-            A.at<double>(2 * i, 6) = -origin[i].x * transformed[i].x;
-            A.at<double>(2 * i, 7) = -origin[i].y * transformed[i].x;
 
             A.at<double>(2 * i + 1, 3) = origin[i].x;
             A.at<double>(2 * i + 1, 4) = origin[i].y;
             A.at<double>(2 * i + 1, 5) = 1;
-            A.at<double>(2 * i + 1, 6) = -origin[i].x * transformed[i].y;
-            A.at<double>(2 * i + 1, 7) = -origin[i].y * transformed[i].y;
 
             B.at<double>(2 * i, 0) = transformed[i].x;
             B.at<double>(2 * i + 1, 0) = transformed[i].y;
@@ -37,16 +33,18 @@ public:
         Mat X;
         solve(A, B, X, DECOMP_SVD);
 
-        M = Mat(3, 3, CV_64F, Scalar(0));
-        M.at<double>(0, 0) = X.at<double>(0, 0);
-        M.at<double>(0, 1) = X.at<double>(1, 0);
-        M.at<double>(0, 2) = X.at<double>(2, 0);
-        M.at<double>(1, 0) = X.at<double>(3, 0);
-        M.at<double>(1, 1) = X.at<double>(4, 0);
-        M.at<double>(1, 2) = X.at<double>(5, 0);
-        M.at<double>(2, 0) = X.at<double>(6, 0);
-        M.at<double>(2, 1) = X.at<double>(7, 0);
-        M.at<double>(2, 2) = 1;
+        M = Mat(3, 3, CV_64F);
+
+        M.at<double>(0,0) = X.at<double>(0,0);     
+        M.at<double>(0,1) = X.at<double>(1,0);     
+        M.at<double>(0,2) = X.at<double>(2,0);     
+        M.at<double>(1,0) = X.at<double>(3,0);     
+        M.at<double>(1,1) = X.at<double>(4,0);     
+        M.at<double>(1,2) = X.at<double>(5,0);
+        M.at<double>(2,0) = 0;
+        M.at<double>(2,1) = 0;
+        M.at<double>(2,2) = 1;
+        cout<<M;
     }
 
     Point2f transformPoint(const Point2f& point)
@@ -54,8 +52,8 @@ public:
         Mat pointMat = (Mat_<double>(3, 1) << point.x, point.y, 1);
         Mat transformedPoint = M * pointMat;
 
-        return Point2f(static_cast<float>(transformedPoint.at<double>(0, 0) / transformedPoint.at<double>(2, 0)),
-                       static_cast<float>(transformedPoint.at<double>(1, 0) / transformedPoint.at<double>(2, 0)));
+        return Point2f(static_cast<float>(transformedPoint.at<double>(0, 0)),
+                       static_cast<float>(transformedPoint.at<double>(1, 0)));
     }
 };
 
@@ -65,7 +63,7 @@ int main()
 
     vector<Point2f> origin = {Point2f(173, 284), Point2f(477, 33), Point2f(248, 455), Point2f(553, 193)};
     vector<Point2f> transformed = {Point2f(100, 100), Point2f(412, 100), Point2f(100, 412), Point2f(412, 412)};
-    TransformFunction func(origin, transformed);
+    AffineTransform transform(origin, transformed);
 
     Mat transformed_img(512, 512, input_img.type());
 
@@ -73,7 +71,7 @@ int main()
     {
         for (int y = 0; y < input_img.cols; y++)
         {
-            Point2f transformed_point = func.transformPoint(Point2f(y, x));
+            Point2f transformed_point = transform.transformPoint(Point2f(y, x));
 
             int t_x = static_cast<int>(transformed_point.y);
             int t_y = static_cast<int>(transformed_point.x);
